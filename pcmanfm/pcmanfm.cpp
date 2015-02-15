d9433ee32da740be71b93318f6924aca54549d06
Alf Gaida <agaida@siduction.org>
2015-02-15 23:34:16 +0100

    don't track orig source

--- pcmanfm/pcmanfm.cpp
+++
@@ -1,9 +0,0 @@
-#include <libfm/fm.h>
-#include "application.h"
-#include "libfmqt.h"
-
-int main(int argc, char** argv) {
-  PCManFM::Application app(argc, argv);
-  app.init();
-  return app.exec();
-}
